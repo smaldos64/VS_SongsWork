814b616872f5498c10c72090313ea9cd2c0ae0cc
Lars Thise Pedersen <ltpe@techcollege.dk>
2019-01-02 15:41:45 +0100

    Gemmer filer før Database ændring.

--- SongsWork/Controllers/SongController.cs
+++ SongsWork/Controllers/SongController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using SongsWork.Models;
+using SongWork.Tools;
 
 namespace SongsWork.Controllers
 {
@@ -12,11 +13,11 @@ namespace SongsWork.Controllers
         private DatabaseContext db = new DatabaseContext();
 
         // GET: Song
-        public ActionResult Index()
-        {
-            List<Song> SongList = db.Songs.ToList(); 
-            return View(SongList);
-        }
+        //public ActionResult Index()
+        //{
+        //    List<Song> SongList = db.Songs.ToList(); 
+        //    return View(SongList);
+        //}
 
         public ActionResult SongList()
         {
@@ -31,6 +32,82 @@ namespace SongsWork.Controllers
             if (null != Song_Object)
             {
                 return (View(Song_Object));
+            }
+            else
+            {
+                return (RedirectToAction("Index", "Home"));
+            }
+        }
+
+        public ActionResult DeleteSong(int Id)
+        {
+            Song Song_Object = db.Songs.Find(Id);
+
+            if (null != Song_Object)
+            {
+                db.Songs.Remove(Song_Object);
+                db.SaveChanges();
+            }
+            return (RedirectToAction("Songlist", "Song"));
+        }
+
+        public ActionResult CreateSong()
+        {
+            List<Genre> GenreList = db.Genres.ToList();
+
+            return (View(GenreList));
+        }
+
+        [HttpPost]
+        public ActionResult CreateSong(Song Song_Object)
+        {
+            int NumberOfRecordsSaved = 0;
+
+            if (ModelState.IsValid)
+            {
+                Song_Object.SongTitle =
+                    StringHandlingTools.ToLowercaseAndSpecifiedCharacterToUpper(Song_Object.SongTitle, ' ');
+
+                db.Songs.Add(Song_Object);
+                NumberOfRecordsSaved = db.SaveChanges();
+
+                if (NumberOfRecordsSaved > 0)
+                {
+                    return (RedirectToAction("SongList", "Song"));
+                }
+                else
+                {
+                    return (RedirectToAction("Index", "Home"));
+                }
+            }
+            else
+            {
+                return (View());
+            }
+        }
+
+        public ActionResult EditSong(int Id)
+        {
+            Song Song_Object = db.Songs.Find(Id);
+            ViewBag.GenreList = db.Genres.ToList();
+
+            return (View(Song_Object));
+        }
+
+        [HttpPost]
+        public ActionResult EditSong(Song Song_Object)
+        {
+            int NumberOfRecordsSaved = 0;
+
+            Song Song_ObjectSave = db.Songs.Find(Song_Object.SongID);
+            Song_ObjectSave.GenreID = Song_Object.GenreID;
+            Song_ObjectSave.SongTitle = StringHandlingTools.ToLowercaseAndSpecifiedCharacterToUpper(Song_Object.SongTitle, ' ');
+           
+            NumberOfRecordsSaved = db.SaveChanges();
+
+            if (NumberOfRecordsSaved > 0)
+            {
+                return (RedirectToAction("SongList", "Song"));
             }
             else
             {
---
+++ SongsWork/Tools/StringHandlingTools.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace SongWork.Tools
+{
+    public class StringHandlingTools
+    {
+        public static string RemoveMultipleSpaces(string Input)
+        {
+            Input = Regex.Replace(Input, @"\s+", " ");
+
+            return (Input);
+        }
+
+        public static string RemoveSpaces(string Input)
+        {
+            Input = Input.Replace(" ", String.Empty);
+            
+            return (Input);
+        }
+
+        public static string RemoveAllNonLetterCharactersFromString(string Input)
+        {
+            Input = new string((from c in Input
+                                where char.IsWhiteSpace(c) || char.IsLetterOrDigit(c)
+                                select c
+                                ).ToArray());
+
+            return (Input);
+        }
+
+        public static string RemoveNumbersFromString(string Input)
+        {
+            Input = Regex.Replace(Input, "[0-9]", "");
+
+            return (Input);
+        }
+
+        public static string ToUpperAfterSpecifiedChar(char SearchCharacter,
+                                                       string Input)
+        {
+            int SearchPosition = Input.IndexOf(SearchCharacter);
+
+            if (SearchPosition > 0)
+            {
+                Input = Input.Substring(0, SearchPosition + 1) +
+                        Input[SearchPosition + 1].ToString().ToUpper() +
+                        Input.Substring(SearchPosition + 2);
+            }
+
+            return (Input);
+        }
+
+        public static string FirstCharacterInStringToUpper(string Input)
+        {
+            Input = Input[0].ToString().ToUpper() + Input.Substring(1);
+
+            return (Input);
+        }
+
+        public static string ToLowercaseAndSpecifiedCharacterToUpper(string Input,
+                                                                     char SpecifiedCharacter)
+        { 
+            Input = Input.Trim();
+            Input = Input.ToLower();
+            Input = RemoveNumbersFromString(Input);
+            Input = RemoveAllNonLetterCharactersFromString(Input);
+            Input = RemoveMultipleSpaces(Input);
+            Input = Input[0].ToString().ToUpper() + Input.Substring(1);
+            Input = ToUpperAfterSpecifiedChar(SpecifiedCharacter, Input);
+
+            return (Input);
+        }
+
+        public static string ToUpperCaseAndRemoveUnwantedCharacters(string Input)
+        {
+            Input = Input.Trim();
+            Input = Input.ToUpper();
+            Input = RemoveAllNonLetterCharactersFromString(Input);
+            Input = RemoveSpaces(Input);
+
+            return (Input);
+        }
+
+        public static string FirstToUpperCaseAndRemoveUnwantedCharacters(string Input)
+        {
+            Input = Input.Trim();
+            Input = Input.ToLower();
+            Input = RemoveAllNonLetterCharactersFromString(Input);
+            Input = RemoveMultipleSpaces(Input);
+            Input = ToUpperAfterSpecifiedChar(' ', Input);
+            Input = FirstCharacterInStringToUpper(Input);
+
+            return (Input);
+        }
+    }
+}
